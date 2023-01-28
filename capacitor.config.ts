3fe40a68e91d90255ebe6e0cb5a4ad7306b822a0
Zahid <zi@vikingc.com>
2023-01-28 09:25:48 +0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'my.first',
+  appName: 'my-first',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
